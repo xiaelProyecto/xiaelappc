2a7f6ed818792884227f0c940756f0db6499b00e
xiaelProyecto <104699669+xiaelProyecto@users.noreply.github.com>
2022-05-24 00:39:34 +0200

    responsive

--- Commons/Commons.cs
+++ Commons/Commons.cs
@@ -8,31 +8,62 @@ namespace xiaelappc.Commons
     public class Commons
     {
 
+        ////API CALLS TO LOCALHOST
+        ////MOVIE
+        //public const string _URIMOVIES_GETALLMOVIES = "https://xiaelapi.azurewebsites.net/api/Movie/GetAllMovies";
+        //public const string _URIMOVIES_GETBYID = "https://xiaelapi.azurewebsites.net/api/Movie/GetMovieById/";
+        //public const string _URIMOVIES_GETBYNAME = "https://xiaelapi.azurewebsites.net/api/Movie/SearchService/";
+        ////USERS
+        //public const string _URIUSERS_GETALLUSERS = "https://xiaelapi.azurewebsites.net/api/User/Get";
+        //public const string _URIUSERS_GETBYID = "https://xiaelapi.azurewebsites.net/api/User/GetById/";
+        //public const string _URIUSERS_UPDATEDESCRIPTION = "https://xiaelapi.azurewebsites.net/api/User/UpdateUserDesc/";
+        //public const string _URIUSERS_UPDATEAGE = "https://xiaelapi.azurewebsites.net/api/User/UpdateUserAge/";
+        //public const string _URIUSERS_UPDATENICK = "https://xiaelapi.azurewebsites.net/api/User/UpdateUserNick/";
+        //public const string _URIUSERS_UPDATEMAIL = "https://xiaelapi.azurewebsites.net/api/User/UpdateUserMail/";
+        //public const string _URIUSERS_UPDATEPASS = "https://xiaelapi.azurewebsites.net/api/User/UpdateUserPass/";
+        //public const string _URIUSERS_UPDATEFAVM = "https://xiaelapi.azurewebsites.net/api/User/UpdateUserFavm/";
+        //public const string _URIUSERS_UPDATEFAVA = "https://xiaelapi.azurewebsites.net/api/User/UpdateUserFava/";
+        //public const string _URIUSERS_CREATEUSER = "https://xiaelapi.azurewebsites.net/api/User/createUser/";
+        //public const string _URIUSERS_DELETE = "https://xiaelapi.azurewebsites.net/api/User/Delete/";
+
+        ////LOGINACCESS
+        //public const string _URILOGIN_LOGIN = "https://xiaelapi.azurewebsites.net/api/LoginAcces/Login";
+        ////ANIME
+        //public const string _URIANIME_GETALLANIMES = "https://xiaelapi.azurewebsites.net/api/Anime/GetAllAnimes";
+        //public const string _URIANIME_GETANIMEBYID = "https://xiaelapi.azurewebsites.net/api/Anime/GetAnimeById/";
+        //public const string _URIANIME_GETBYNAME = "https://xiaelapi.azurewebsites.net/api/Anime/SearchService/";
+
+
+
+
+
+        //PARA XIANG
+
+
         //API CALLS TO LOCALHOST
         //MOVIE
-        public const string _URIMOVIES_GETALLMOVIES = "https://xiaelapi.azurewebsites.net/api/Movie/GetAllMovies";
-        public const string _URIMOVIES_GETBYID = "https://xiaelapi.azurewebsites.net/api/Movie/GetMovieById/";
-        public const string _URIMOVIES_GETBYNAME = "https://xiaelapi.azurewebsites.net/api/Movie/SearchService/";
+        public const string _URIMOVIES_GETALLMOVIES = "https://localhost:44327/api/Movie/GetAllMovies";
+        public const string _URIMOVIES_GETBYID = "https://localhost:44327/api/Movie/GetMovieById/";
+        public const string _URIMOVIES_GETBYNAME = "https://localhost:44327/api/Movie/SearchService/";
         //USERS
-        public const string _URIUSERS_GETALLUSERS = "https://xiaelapi.azurewebsites.net/api/User/Get";
-        public const string _URIUSERS_GETBYID = "https://xiaelapi.azurewebsites.net/api/User/GetById/";
-        public const string _URIUSERS_UPDATEDESCRIPTION = "https://xiaelapi.azurewebsites.net/api/User/UpdateUserDesc/";
-        public const string _URIUSERS_UPDATEAGE = "https://xiaelapi.azurewebsites.net/api/User/UpdateUserAge/";
-        public const string _URIUSERS_UPDATENICK = "https://xiaelapi.azurewebsites.net/api/User/UpdateUserNick/";
-        public const string _URIUSERS_UPDATEMAIL = "https://xiaelapi.azurewebsites.net/api/User/UpdateUserMail/";
-        public const string _URIUSERS_UPDATEPASS = "https://xiaelapi.azurewebsites.net/api/User/UpdateUserPass/";
-        public const string _URIUSERS_UPDATEFAVM = "https://xiaelapi.azurewebsites.net/api/User/UpdateUserFavm/";
-        public const string _URIUSERS_UPDATEFAVA = "https://xiaelapi.azurewebsites.net/api/User/UpdateUserFava/";
-        public const string _URIUSERS_CREATEUSER = "https://xiaelapi.azurewebsites.net/api/User/createUser/";
-        public const string _URIUSERS_DELETE = "https://xiaelapi.azurewebsites.net/api/User/Delete/";
+        public const string _URIUSERS_GETALLUSERS = "https://localhost:44327/api/User/Get";
+        public const string _URIUSERS_GETBYID = "https://localhost:44327/api/User/GetById/";
+        public const string _URIUSERS_UPDATEDESCRIPTION = "https://localhost:44327/api/User/UpdateUserDesc/";
+        public const string _URIUSERS_UPDATEAGE = "https://localhost:44327/api/User/UpdateUserAge/";
+        public const string _URIUSERS_UPDATENICK = "https://localhost:44327/api/User/UpdateUserNick/";
+        public const string _URIUSERS_UPDATEMAIL = "https://localhost:44327/api/User/UpdateUserMail/";
+        public const string _URIUSERS_UPDATEPASS = "https://localhost:44327/api/User/UpdateUserPass/";
+        public const string _URIUSERS_UPDATEFAVM = "https://localhost:44327/api/User/UpdateUserFavm/";
+        public const string _URIUSERS_UPDATEFAVA = "https://localhost:44327/api/User/UpdateUserFava/";
+        public const string _URIUSERS_CREATEUSER = "https://localhost:44327/api/User/createUser/";
+        public const string _URIUSERS_DELETE = "https://localhost:44327/api/User/Delete/";
 
         //LOGINACCESS
-        public const string _URILOGIN_LOGIN = "https://xiaelapi.azurewebsites.net/api/LoginAcces/Login";
+        public const string _URILOGIN_LOGIN = "https://localhost:44327/api/LoginAcces/Login";
         //ANIME
-        public const string _URIANIME_GETALLANIMES = "https://xiaelapi.azurewebsites.net/api/Anime/GetAllAnimes";
-        public const string _URIANIME_GETANIMEBYID = "https://xiaelapi.azurewebsites.net/api/Anime/GetAnimeById/";
-        public const string _URIANIME_GETBYNAME = "https://xiaelapi.azurewebsites.net/api/Anime/SearchService/";
+        public const string _URIANIME_GETALLANIMES = "https://localhost:44327/api/Anime/GetAllAnimes";
+        public const string _URIANIME_GETANIMEBYID = "https://localhost:44327/api/Anime/GetAnimeById/";
+        public const string _URIANIME_GETBYNAME = "https://localhost:44327/api/Anime/SearchService/";
 
-        
     }
 }
